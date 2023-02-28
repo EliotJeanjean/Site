47889663e04155b972ec0f91ee2ae91bf48ef2e0
Tuttle <102301719+EliotJeanjean@users.noreply.github.com>
2023-03-01 00:07:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const navLinks = document.querySelectorAll('.main_nav a');
 const contentDivs = document.querySelectorAll('.content');
 const button = document.querySelector("agrandir");
-const box = document.querySelector(".content_display");
 
 button.addEventListener("click", () => {
   box.classList.toggle("toggle");
@@ -17,3 +16,16 @@ navLinks.forEach(function(link) {
     activeDiv.classList.add('active');
   });
 });
+const box = document.querySelector(".content_display");
+var switch = 0, element = document.getElementById("agrandir"), img1, img2;
+element.onclick = function(){
+
+            if (switch == 0){
+                element.style.backgroundImage(img1);
+                switch = 1;
+                }
+            else {
+                        element.style.backgroundImage(img2);
+                switch = 0
+                }
+                }
