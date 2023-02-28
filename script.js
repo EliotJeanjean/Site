2bcbc793420f95b5829a6e81bb4c89c5888ccd3d
Tuttle <102301719+EliotJeanjean@users.noreply.github.com>
2023-02-28 19:10:13 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const navLinks = document.querySelectorAll('.main_nav a');
+const contentDivs = document.querySelectorAll('.content');
+
+navLinks.forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    const divId = this.dataset.div;
+    contentDivs.forEach(function(div) {
+      div.classList.remove('active');
+    });
+    const activeDiv = document.getElementById(divId);
+    activeDiv.classList.add('active');
+  });
+});
