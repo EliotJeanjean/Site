70d4c6161f16f23fc61ffd5d710ef0c6855fba56
Tuttle <102301719+EliotJeanjean@users.noreply.github.com>
2023-02-28 23:30:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const navLinks = document.querySelectorAll('.main_nav a');
 const contentDivs = document.querySelectorAll('.content');
+const box = document.querySelector(".content_display")
+const button = document.querySelector(".agrandir")
 
+button.addEventListener("click", () => {
+  box.classList.toggle("toggle")
+})
 navLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
@@ -11,22 +16,4 @@ navLinks.forEach(function(link) {
     const activeDiv = document.getElementById(divId);
     activeDiv.classList.add('active');
   });
-});
-var button = document.querySelector("#width_button");
-const button = document.getElementById('toggle_button');
-
-function toggleWidth() {
-  if (content_display.classList.contains('content_display_small')) {
-    content_display.classList.remove('content_display_small');
-    content_display.classList.add('content_display_large');
-    button.innerHTML = "Reduce Width";
-  } else {
-    content_display.classList.remove('content_display_large');
-    content_display.classList.add('content_display_small');
-    button.innerHTML = "Enlarge Width";
-  }
-}
-
-button.addEventListener('click', function() {
-  toggleWidth();
 });
