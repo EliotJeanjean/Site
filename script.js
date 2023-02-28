e0a78a015eef4e48b1dd46e83b835d2070052a3e
Tuttle <102301719+EliotJeanjean@users.noreply.github.com>
2023-02-28 22:25:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const navLinks = document.querySelectorAll('.main_nav a');
 const contentDivs = document.querySelectorAll('.content');
-const button = document.getElementById('agrandir');
 const contentDisplay = document.getElementById('content_display');
 
 navLinks.forEach(function(link) {
@@ -15,16 +14,22 @@ navLinks.forEach(function(link) {
   });
 });
 
-button.addEventListener('click', function() {
-  if (button.classList.contains('agrandir')) {
-    contentDisplay.style.width = '80%';
-    contentDisplay.style.transition = 'width 0.5s ease-out';
-    button.innerHTML = 'Réduire';
-    button.classList.remove('agrandir');
-  } else {
-    contentDisplay.style.width = '50%';
-    contentDisplay.style.transition = 'width 0.5s ease-out';
-    button.innerHTML = 'Agrandir';
-    button.classList.add('agrandir');
-  }
-});
+function changeButtonState() {
+  let button = document.getElementById("agrandir");
+
+  button.addEventListener("click", function() {
+    if (button.classList.contains("agrandir")) {
+      contentDisplay.style.width = "80%";
+      contentDisplay.style.transition = "width 0.5s ease-out";
+      button.innerHTML = "Réduire";
+      button.classList.remove("agrandir");
+    } else {
+      contentDisplay.style.width = "50%";
+      contentDisplay.style.transition = "width 0.5s ease-out";
+      button.innerHTML = "Agrandir";
+      button.classList.add("agrandir");
+    }
+  });
+}
+
+changeButtonState();
