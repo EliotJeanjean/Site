a81b20898ca73bcad28174bdf26febf1838407a2
Tuttle <102301719+EliotJeanjean@users.noreply.github.com>
2023-02-28 23:47:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const navLinks = document.querySelectorAll('.main_nav a');
 const contentDivs = document.querySelectorAll('.content');
-const box = document.querySelector(".content_display")
-const button = document.querySelector("button")
+const button = document.querySelector("button");
+const box = document.querySelector(".content_display");
 
 button.addEventListener("click", () => {
-  box.classList.toggle("toggle")
-})
+  box.classList.toggle("toggle");
+});
 navLinks.forEach(function(link) {
   link.addEventListener('click', function(event) {
     event.preventDefault();
