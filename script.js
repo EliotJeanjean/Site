eef14099b1b4d6e57e1c95d7fbb0e20c57e748c2
Tuttle <102301719+EliotJeanjean@users.noreply.github.com>
2023-02-28 20:38:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,9 +18,6 @@ const contentDisplay = document.querySelector('.content_display');
 toggleWidthBtn.addEventListener('click', () => {
   contentDisplay.classList.toggle('content_display--full');
 });
-const toggleWidthBtn = document.querySelector('#toggle-width-btn');
-const contentDisplay = document.querySelector('.content_display');
-
 toggleWidthBtn.addEventListener('click', () => {
   if (contentDisplay.classList.contains('content_display--full')) {
     contentDisplay.classList.remove('content_display--full');
